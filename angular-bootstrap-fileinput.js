b9a3d4d7dce367d5da7d13b72e4b694270d8b349
Gerald Humphries <geraldhumphries@gmail.com>
2016-04-24 19:08:10 -0400

    initial commit

---
+++ angular-bootstrap-fileinput.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('angularBootstrapFileinput', [])
+        .directive('angularBootstrapFileinput', angularBootstrapFileinput);
+
+    function angularBootstrapFileinput() {
+        var directive = {
+            restrict: 'A',
+            scope: {
+                options: '=angularBfi'
+            },
+            link: linkFunc
+        };
+
+        return directive;
+
+        function linkFunc(scope, element) {
+            element.fileinput(scope.options);
+            scope.$watch('options', refresh, true);
+
+            function refresh() {
+                element.fileinput('refresh', scope.options);
+            }
+        }
+    }
+})();
